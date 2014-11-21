5007ac5df1d0d34cab71dfc1d098d0dcd9ad4aef
martinchu <martinchu09@gmail.com>
2014-11-20 20:03:31 -0500

    Create player.h

---
+++ player.h
@@ -0,0 +1,33 @@
+#include <string>
+
+class Player{
+protected:
+		int money;
+		int debt
+		char piece;
+		int place;
+		map <std::string,int> monoplyBlock;
+		std::vector<Property *> properties;
+		bool isBankrupted();
+public:
+	void roll();
+	void showAssets();
+	void declareBankruptcy();
+	void give(Property &prop,Player &p);
+	void receive(Property &prop);
+	void mortgageProp(std::string propname);
+	void unmortgageProp(std::string propname);
+	void buyImproveProp(std::string propname);
+	void sellImproveProp(std::string propname);
+};
+
+class Human: public Player{
+public:
+	Human(std::string Name);
+};
+
+class PC:public Player{
+public:
+	PC(std::string Name);
+
+};

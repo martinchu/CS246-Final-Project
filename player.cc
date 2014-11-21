ec477498fabe8697ef66fa823efeae62ff067c35
martinchu <martinchu09@gmail.com>
2014-11-20 20:01:54 -0500

    Create player.cc

---
+++ player.cc
@@ -0,0 +1,29 @@
+#include <cstdlib>
+void give(Property &prop,Player &p){
+	p.receive(prop);
+	this.remove(prop);
+}
+void receive(Property &prop){
+	this.add(prop);
+}
+void buyImproveProp(string propname){
+	//find the prop under the array
+	properties[i].buyImprove();
+}
+void sellImproveProp(string propname){
+	properties[i].sellImprove();
+}
+
+void roll(){
+	
+}
+void showAssets(){
+	for loop
+		cout<<;
+}
+bool isBankrupted(){
+	if(debt>money){
+		return true;
+	}
+	return false;
+}
